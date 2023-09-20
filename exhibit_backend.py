b6ae43137c06d20d3c2bdd3be1d21476c0421126
Lucien Thomas <lthomas@sciport.org>
2023-09-20 16:34:59 -0500

    Fourth Method

--- exhibit_backend.py
+++ exhibit_backend.py
@@ -9,40 +9,43 @@ app = Flask(__name__)
 CORS(app)
 
 GPIO_PIN = 14
-DEBOUNCE_TIME = 50  # in milliseconds, adjust as needed
+DEBOUNCE_TIME = 10  # in milliseconds, adjust as needed
 NUM_READINGS = 2  # Number of readings to take before averaging
 last_pulse_time = 0  # To counter continueous triggers (sensor stopped over magnet)
+pulse_times = []  # Global list to store pulse times
 
 GPIO.setmode(GPIO.BCM)
 GPIO.setup(GPIO_PIN, GPIO.IN, pull_up_down=GPIO.PUD_UP)
 
+
 def get_rpm():
+    global pulse_times
+    MAX_STORED_TIMES = 10  # Number of pulse times to store
+    STOPPED_THRESHOLD = 0.02  # If average time between pulses is below this, engine is considered stopped. Adjust as needed.
+    
     pulse_count = 0
-    continuous_trigger_time = 0
-    start_time = time.time()
-    end_time = start_time + 1  # We'll measure for one second
+    current_time = time.time()
     last_detected_time = 0  # Track time of last detected pulse in the loop
 
-    while time.time() < end_time:
+    while current_time - last_detected_time < 1:  # We'll measure for one second from the last detected pulse
         current_time = time.time()
-
-        if GPIO.input(GPIO_PIN) == 0:  # Detecting a falling edge (magnet passing by)
-            # Check if the time between two consecutive pulses is greater than debounce time
-            print(f"Pulse detected. Time since last: {current_time - last_detected_time}")
-            if current_time - last_detected_time > DEBOUNCE_TIME/1000:
-                pulse_count += 1
-            else:
-                continuous_trigger_time += current_time - last_detected_time
+        
+        if GPIO.input(GPIO_PIN) == 0:
+            pulse_count += 1
+            
+            # Store pulse time and maintain list length
+            pulse_times.append(current_time)
+            if len(pulse_times) > MAX_STORED_TIMES:
+                pulse_times.pop(0)
+            
+            # Calculate average time between pulses
+            if len(pulse_times) > 1:
+                avg_time_between_pulses = (pulse_times[-1] - pulse_times[0]) / (len(pulse_times) - 1)
+                if avg_time_between_pulses < STOPPED_THRESHOLD:
+                    return 0
+            
             last_detected_time = current_time
-
-            # If continuously triggered for more than 2 seconds, consider engine as stopped
-            if continuous_trigger_time > 2:
-                return 0
-
             time.sleep(DEBOUNCE_TIME/1000)  # Debounce for the given time
-        else:
-            # Reset the counter if not being continuously triggered
-            continuous_trigger_time = 0
 
     rpm = (pulse_count / 7) * 60  # Calculate RPM
     return rpm
