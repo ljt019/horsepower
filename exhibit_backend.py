d98c119563a078b3e2824bf6a26ca97b64c6ba16
Lucien Thomas <lthomas@sciport.org>
2023-10-04 11:22:43 -0500

    Backend changes

--- exhibit_backend.py
+++ exhibit_backend.py
@@ -4,51 +4,79 @@ from flask import Flask, jsonify, render_template
 from flask_cors import CORS
 import RPi.GPIO as GPIO
 import time
+import logging
+
+logging.basicConfig(filename='backend.log', filemode='w', level=logging.INFO, 
+                    format='%(asctime)s - %(levelname)s - %(message)s')
 
 app = Flask(__name__)
 CORS(app)
 
+
 GPIO_PIN = 14
 DEBOUNCE_TIME = 10  # in milliseconds, adjust as needed
-NUM_READINGS = 2  # Number of readings to take before averaging
+RPM_SAMPLE_SIZE = 2  # Number of readings to take before averaging
+
 last_pulse_time = 0  # To counter continueous triggers (sensor stopped over magnet)
 pulse_times = []  # Global list to store pulse times
+
+logging.info("-------------------CONSTANTS-------------------")
+logging.info(f"GPIO_PIN: {GPIO_PIN}, DEBOUNCE_TIME: {DEBOUNCE_TIME}, RPM_SAMPLE_SIZE: {RPM_SAMPLE_SIZE}")
 
 GPIO.setmode(GPIO.BCM)
 GPIO.setup(GPIO_PIN, GPIO.IN, pull_up_down=GPIO.PUD_UP)
 
-
-def get_rpm():
-    global pulse_times
-    MAX_STORED_TIMES = 10  # Number of pulse times to store
-    STOPPED_THRESHOLD = 0.02  # If average time between pulses is below this, engine is considered stopped. Adjust as needed.
-    
+def initialize_rpm_variables():
     pulse_count = 0
     current_time = time.time()
-    last_detected_time = 0  # Track time of last detected pulse in the loop
+    last_detected_time = 0
+    return pulse_count, current_time, last_detected_time
 
-    while current_time - last_detected_time < 1:  # We'll measure for one second from the last detected pulse
+def update_pulse_times(pulse_times, current_time, MAX_STORED_TIMES):
+    pulse_times.append(current_time)
+    if len(pulse_times) > MAX_STORED_TIMES:
+        pulse_times.pop(0)
+    return pulse_times
+
+def calculate_average_pulse_interval(pulse_times):
+    if len(pulse_times) > 1:
+        return (pulse_times[-1] - pulse_times[0]) / (len(pulse_times) - 1)
+    return None
+
+def is_engine_stopped(avg_time_between_pulses, STOPPED_THRESHOLD):
+    return avg_time_between_pulses < STOPPED_THRESHOLD if avg_time_between_pulses is not None else False
+
+def calculate_rpm(pulse_count):
+    return (pulse_count / 7) * 60
+
+logging.info("-------------------Measured RPM-------------------")
+def get_rpm(GPIO_PIN, DEBOUNCE_TIME, MAX_STORED_TIMES=10, STOPPED_THRESHOLD=0.1):
+    global pulse_times  
+    pulse_count, current_time, last_detected_time = initialize_rpm_variables()
+
+    while current_time - last_detected_time < 1:
         current_time = time.time()
         
-        if GPIO.input(GPIO_PIN) == 0:
-            pulse_count += 1
-            
-            # Store pulse time and maintain list length
-            pulse_times.append(current_time)
-            if len(pulse_times) > MAX_STORED_TIMES:
-                pulse_times.pop(0)
-            
-            # Calculate average time between pulses
-            if len(pulse_times) > 1:
-                avg_time_between_pulses = (pulse_times[-1] - pulse_times[0]) / (len(pulse_times) - 1)
-                if avg_time_between_pulses < STOPPED_THRESHOLD:
+        try:
+            if GPIO.input(GPIO_PIN) == 0:
+                pulse_count += 1
+                pulse_times = update_pulse_times(pulse_times, current_time, MAX_STORED_TIMES)
+                
+                avg_time_between_pulses = calculate_average_pulse_interval(pulse_times)
+                if is_engine_stopped(avg_time_between_pulses, STOPPED_THRESHOLD):
+                    logging.info("Detected engine as stopped.")
                     return 0
-            
-            last_detected_time = current_time
-            time.sleep(DEBOUNCE_TIME/1000)  # Debounce for the given time
-
-    rpm = (pulse_count / 7) * 60  # Calculate RPM
+                
+                last_detected_time = current_time
+                time.sleep(DEBOUNCE_TIME / 1000)
+        except Exception as e:
+            logging.info("-------------------Errors-------------------")
+            logging.error(f"An error occurred: {e}")
+    
+    rpm = calculate_rpm(pulse_count)
+    logging.debug(f"Detected RPM: {rpm}")
     return rpm
+
 
 @app.route("/")
 def index():
@@ -58,11 +86,11 @@ def index():
 def get_horsepower():
     total_rpm = 0
 
-    for _ in range(NUM_READINGS):
+    for _ in range(RPM_SAMPLE_SIZE):
         total_rpm += get_rpm()
         time.sleep(0.1)  # Sleep a bit between readings
 
-    avg_rpm = total_rpm / NUM_READINGS
+    avg_rpm = total_rpm / RPM_SAMPLE_SIZE
     torque = 14  # fixed at 16 foot-pounds
     horsepower = (torque * avg_rpm) / 5252
 
